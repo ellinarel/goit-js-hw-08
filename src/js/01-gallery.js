1bda74533ca2a157c003eb5b29be003f66077365
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-02-19 02:36:24 +0200

    2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery")
+const imgCardMarkup = createimgCardMarkup(galleryItems)
+function createimgCardMarkup(galleryItems) 
+{return galleryItems.map(({preview, original, description})=> 
+`<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}"" alt="${description}"/>
+</a>`)
+    .join("")
+}
+galleryEl.insertAdjacentHTML('afterbegin', imgCardMarkup);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {  captionDelay: 250,
+    captionsData: "alt",})
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state'
+const formData = {}
+const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY))
+
+const form = document.querySelector(".feedback-form")
+
+form.addEventListener("submit", onFormSubmit)
+form.addEventListener("input", throttle(onFormData,500))
+
+populateTextarea()
+
+function onFormData(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+
+function onFormSubmit(e){
+e.preventDefault();
+console.log(parsedData)
+console.log('Отправляем форму');
+e.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY)
+}
+
+function populateTextarea(){
+const data = parsedData;
+const email = document.querySelector('.feedback-form input');;
+const message = document.querySelector('.feedback-form textarea');
+if (data) {
+    return (
+  (email.value = data.email || ' ' ) ,
+ (message.value = data.message || ' ')
+ )
+}}
+;
